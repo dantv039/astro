0878ebe43d739b9c8f85b716905aaa329d7f772f
dantv039 <100129873+dantv039@users.noreply.github.com>
2023-03-01 23:37:38 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnjlgz8ywdn0ey -r community-pools.mysrv.cloud:10300 -p rpc;
+    ./astrominer -w dero1qynkk46da26s9t6fqdushtqlzfpuwd94fznc88hqq9snpat2600x7qgq53jf8 -r 192.168.1.18:10100 -r1 community-pools.mysrv.cloud:10300 -r2 dero-node-sg.mysrv.cloud:10300 -p rpc;
     sleep 5;
 done
